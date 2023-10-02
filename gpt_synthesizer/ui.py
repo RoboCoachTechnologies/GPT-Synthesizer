45e993906f4cd89596c8a5e467d0ff747e34a17c
Behrad Rabiei <brabiei@ucsd.edu>
2023-10-02 00:54:45 -0700

    Ready for final revisions.

--- gpt_synthesizer/ui.py
+++ gpt_synthesizer/ui.py
@@ -88,54 +88,10 @@ def format_message(text):
 
     return formatted_text
 
-
-def message_func(text, is_user=False):
-    """
-    This function is used to display the messages in the chatbot UI.
-
-    Parameters:
-    text (str): The text to be displayed.
-    is_user (bool): Whether the message is from the user or not.
-    is_df (bool): Whether the message is a dataframe or not.
-    """
-    if is_user:
-        avatar_url = "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Prescription01&hairColor=Auburn&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=PastelBlue&eyeType=Squint&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Tanned"
-        message_alignment = "flex-end"
-        message_bg_color = "linear-gradient(135deg, #00B2FF 0%, #006AFF 100%)"
-        avatar_class = "user-avatar"
-        st.write(
-            f"""
-                <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
-                    <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px;">
-                        {text} \n </div>
-                    <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
-                </div>
-                """,
-            unsafe_allow_html=True,
-        )
-    else:
-        avatar_url = "https://avataaars.io/?avatarStyle=Transparent&topType=WinterHat2&accessoriesType=Kurt&hatColor=Blue01&facialHairType=MoustacheMagnum&facialHairColor=Blonde&clotheType=Overall&clotheColor=Gray01&eyeType=WinkWacky&eyebrowType=SadConcernedNatural&mouthType=Sad&skinColor=Light"
-        message_alignment = "flex-start"
-        message_bg_color = "#71797E"
-        avatar_class = "bot-avatar"
-
-        
-        text = format_message(text)
-
-        st.write(
-            f"""
-                <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
-                    <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
-                    <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px;">
-                        {text} \n </div>
-                </div>
-                """,
-            unsafe_allow_html=True,
-        )
 def print_user(text):
-    avatar_url = "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Prescription01&hairColor=Auburn&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=PastelBlue&eyeType=Squint&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Tanned"
+    avatar_url = "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads02&accessoriesType=Prescription01&hairColor=BrownDark&facialHairType=Blank&clotheType=ShirtScoopNeck&clotheColor=PastelYellow&eyeType=WinkWacky&eyebrowType=RaisedExcited&mouthType=ScreamOpen&skinColor=Light"
     message_alignment = "flex-end"
-    message_bg_color = "linear-gradient(135deg, #00B2FF 0%, #006AFF 100%)"
+    message_bg_color = "#29D693"
     avatar_class = "user-avatar"
     st.write(
         f"""
@@ -150,8 +106,9 @@ def print_user(text):
 
 def print_bot(text):
     avatar_url = 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Wayfarers&hairColor=BrownDark&facialHairType=Blank&clotheType=CollarSweater&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Pale'
+    #avatar_url = 'https://new.express.adobe.com/tools/animate-from-audio?afaBackgroundId=BG_Citypark.jpg&afaBackgroundCategory=all&afaSize=InstagramSquare&afaPuppetId=Robot+VQA.puppet&afaPuppetCategory=all&afaPuppetScale=1&afaPuppetOffset=0%2C0'
     message_alignment = "flex-start"
-    message_bg_color = "#71797E"
+    message_bg_color = "#D6296C"
     avatar_class = "bot-avatar"
 
     
@@ -168,69 +125,7 @@ def print_bot(text):
         unsafe_allow_html=True,
     )
 
-class StreamlitUICallbackHandler(BaseCallbackHandler):
-    def __init__(self):
-        # Buffer to accumulate tokens
-        self.token_buffer = []
-        self.placeholder = None
-        self.has_streaming_ended = False
-
-    def _get_bot_message_container(self, text):
-        """Generate the bot's message container style for the given text."""
-        avatar_url = "https://avataaars.io/?avatarStyle=Transparent&topType=WinterHat2&accessoriesType=Kurt&hatColor=Blue01&facialHairType=MoustacheMagnum&facialHairColor=Blonde&clotheType=Overall&clotheColor=Gray01&eyeType=WinkWacky&eyebrowType=SadConcernedNatural&mouthType=Sad&skinColor=Light"
-        message_alignment = "flex-start"
-        message_bg_color = "#71797E"
-        avatar_class = "bot-avatar"
-        formatted_text = format_message(text)
-        container_content = f"""
-            <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
-                <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
-                <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px;">
-                    {formatted_text} \n </div>
-            </div>
-        """
-        return container_content
-
-    def on_llm_new_token(self, token, run_id, parent_run_id=None, **kwargs):
-        """
-        Handle the new token from the model. Accumulate tokens in a buffer and update the Streamlit UI.
-        """
-        self.token_buffer.append(token)
-        complete_message = "".join(self.token_buffer)
-        if self.placeholder is None:
-            container_content = self._get_bot_message_container(complete_message)
-            self.placeholder = st.markdown(container_content, unsafe_allow_html=True)
-        else:
-            # Update the placeholder content
-            container_content = self._get_bot_message_container(complete_message)
-            self.placeholder.markdown(container_content, unsafe_allow_html=True)
-
-    def display_dataframe(self, df):
-        """
-        Display the dataframe in Streamlit UI within the chat container.
-        """
-        avatar_url = "https://avataaars.io/?avatarStyle=Transparent&topType=WinterHat2&accessoriesType=Kurt&hatColor=Blue01&facialHairType=MoustacheMagnum&facialHairColor=Blonde&clotheType=Overall&clotheColor=Gray01&eyeType=WinkWacky&eyebrowType=SadConcernedNatural&mouthType=Sad&skinColor=Light"
-        message_alignment = "flex-start"
-        avatar_class = "bot-avatar"
-
-        st.write(
-            f"""
-            <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
-                <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
-        st.write(df)
-
-    def on_llm_end(self, response, run_id, parent_run_id=None, **kwargs):
-        """
-        Reset the buffer when the LLM finishes running.
-        """
-        self.token_buffer = []  # Reset the buffer
-        self.has_streaming_ended = True
-
-
+    
 
 
 
